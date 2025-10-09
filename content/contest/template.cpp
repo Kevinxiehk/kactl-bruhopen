429a77ce844109e37e298309c384aacefd320b91
Kevin Xie <kevinxiehk@gmail.com>
2025-10-09 11:39:46 +0100

    customisation and minor content changes

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 using namespace std;
 
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
@@ -8,7 +10,14 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
+int32_t main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
 }
+
+/*
+./compile.sh
+
+#!/usr/bin/bash
+g++ "${1}.cpp" -o "{$1}" -Dbruh -std=c++20 -O2 -Wall -fsanitize=undefined,address
+*/
